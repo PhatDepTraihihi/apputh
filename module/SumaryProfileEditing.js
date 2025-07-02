8e2dcfdf5a7e2527dbfbcef772249d4a3031b2eb
PhatDepTraihihi <92636905+PhatDepTraihihi@users.noreply.github.com>
2025-07-02 20:25:10 +0700

    Update SumaryProfileEditing.js

--- module/SumaryProfileEditing.js
+++ module/SumaryProfileEditing.js
@@ -1,8 +1,8 @@
 let obj = JSON.parse($response.body);
 
 // Chỉ sửa giá trị trong body
-obj.body.hoDen = Phát;
-obj.body.ten = Đẹp Trai;
+obj.body.hoDem = 'Phát';
+obj.body.ten = 'Đẹp Trai';
 
 // Gửi lại response đã sửa
 $done({ body: JSON.stringify(obj) });
