875f916879fba0d26ef0ba02fad9f30dcf72efc7
PhatDepTraihihi <92636905+PhatDepTraihihi@users.noreply.github.com>
2025-07-02 22:02:28 +0700

    Update DashboardConfigEditing.js

--- module/DashboardConfigEditing.js
+++ module/DashboardConfigEditing.js
@@ -3,6 +3,7 @@ let obj = JSON.parse($response.body);
 // Chỉ sửa giá trị trong body
 obj.body.gpa = 4.00;
 obj.body.studyProgress.current = 120.00;
+obj.body.conductScore.score = 100.00;
 
 // Gửi lại response đã sửa
 $done({ body: JSON.stringify(obj) });
